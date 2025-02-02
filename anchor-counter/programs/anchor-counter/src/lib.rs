7d9a6618a9ddf8187a7ccacfb469ea6d5a6f0e2b
Dhruv Varshney <f20200865@pilani.bits-pilani.ac.in>
2025-02-02 09:40:48 +0000

    chore: added new program id.

--- anchor-counter/programs/anchor-counter/src/lib.rs
+++ anchor-counter/programs/anchor-counter/src/lib.rs
@@ -3,7 +3,7 @@ use ephemeral_rollups_sdk::anchor::{commit, delegate, ephemeral};
 use ephemeral_rollups_sdk::cpi::DelegateConfig;
 use ephemeral_rollups_sdk::ephem::{commit_accounts, commit_and_undelegate_accounts};
 
-declare_id!("9AauSsvebvnBy1buaduvTNteXYzS2DUCdWeHH47fKS3B"); 
+declare_id!("6W6pzBTRn49rn2adaZ9QGcnB627iJtWM2huvXc8RcThB"); 
 
 pub const TEST_PDA_SEED: &[u8] = b"test-pda";
 
